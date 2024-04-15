8d3621de656d5f15aa7d2d5153e8ad00b956034c
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-04-16 02:14:50 +0600

    test-v2

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-module.exports = app;
+app.get("/test", (req, res) => {
+  res.status(200).send("Hello, Zobaidul Kazi");
+});
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import http from "http";
+import app from "./app";
 
-
-
-const http = require('http');
-const app = require('./app');
+// middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 const server = http.createServer(app);
+const port = process.env.PORT || 9900;
 
-server.listen(4000, () => {
-	console.log('Server is running on port 3000');
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
